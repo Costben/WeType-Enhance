30fa76cd23b13b779656079322e81c35b03cc855
Costben <28681450+Costben@users.noreply.github.com>
2026-09-11 14:49:58 +0800

    chore(release): 1.27.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xposed.wetypehook"
         minSdk = 31
         targetSdk = 37
-        versionCode = 33
-        versionName = "1.27.1"
+        versionCode = 34
+        versionName = "1.27.2"
     }
 
     buildTypes {
